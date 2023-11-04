89a727b813c171e15b263ee72c0edd0e457b73b0
narsiala135 <149657591+narsiala135@users.noreply.github.com>
2023-11-04 08:51:54 +0530

    Create Program 7.c

---
+++ Program 7.c
@@ -0,0 +1,67 @@
+[04/11, 8:48 AM] ................A: include <stdio.h>
+int main()
+{
+	int A[100][4]; 			
+	int i, j, n, total = 0, index, temp;
+	float avg_wt, avg_tat;
+	printf("Enter number of process: ");
+	scanf("%d", &n);
+	printf("Enter Burst Time:\n");	
+	for (i = 0; i < n; i++) {
+		printf("P%d: ", i + 1);
+		scanf("%d", &A[i][1]);
+		A[i][0] = i + 1;
+	}
+	for (i = 0; i < n; i++) {
+		index = i;
+		for (j = i + 1; j < n; j++)
+			if (A[j][1] < A[index][1])
+				index = j;
+		temp = A[i][1];
+		A[i][1] = A[index][1];
+		A[index][1] = temp;
+
+		temp = A[i][0];
+		A[i][0] = A[index][0];
+		A[index][0] = temp;
+	}
+	A[0][2] = 0;
+	for (i = 1; i < n; i++) {
+		A[i][2] = 0;
+		for (j = 0; j < i; j++)
+			A[i][2] += A[j][1];
+		total += A[i][2];
+	}
+	avg_wt = (float)total / n;
+	total = 0;
+	printf("P	 BT	 WT	 TAT\n");
+	
+	for (i = 0; i < n; i++) {
+		A[i][3] = A[i][1] + A[i][2];
+		total += A[i][3];
+		printf("P%d	 %d	 %d	 %d\n", A[i][0],A[i][1], A[i][2], A[i][3]);
+	}
+	avg_tat = (float)total / n;
+	printf("Average Waiting Time= %f", avg_wt);
+	printf("\nAverage Turnaround Time= %f", avg_tat);
+}
+[04/11, 8:49 AM] Shiva SU A Slot: include<stdio.h>  
+#include<stdlib.h>  
+#include<unistd.h>  
+#include<sys/shm.h>  
+#include<string.h>  
+int main()  
+{  
+int i;  
+void *shared_memory;  
+char buff[100];  
+int shmid;  
+shmid=shmget((key_t)2345, 1024, 0666|IPC_CREAT);   
+printf("Key of shared memory is %d\n",shmid);  
+shared_memory=shmat(shmid,NULL,0);    
+printf("Process attached at %p\n",shared_memory); 
+printf("Enter some data to write to shared memory\n");  
+read(0,buff,100); 
+strcpy(shared_memory,buff); 
+printf("You wrote : %s\n",(char *)shared_memory);  
+}
